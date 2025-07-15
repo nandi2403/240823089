c6d346b19daed95a937eb77699b7a079a4920409
nandi2403 <nandinishah2411.com>
2025-07-15 11:34:22 +0530

    program 1

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const name="nandini shah"
-console.log(" \nhello"+name)
+// const name="nandini shah"
+// console.log(" \nhello"+name)
+
+
+//15/7/25(function)
+
+let a=5;
+let b=10;
+let sum=a+b;
+
+console.log(`a:${a},b:${b},sum:${sum}`);
+
---
+++ program.js
@@ -0,0 +1,9 @@
+const n = 1
+if(n%2)
+{
+    console.log("odd number")
+}
+else
+{
+    console.log("even number")
+}
